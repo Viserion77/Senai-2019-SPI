78a7a117d8b8b7f2a55e173b5b846ce565484f99
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-07-29 20:29:35 -0300

    Activit 4

---
+++ 1.EstruturaSequencial/L1E4.py
@@ -0,0 +1,6 @@
+score1 = float(input ('Nota 1: '))
+score2 = float(input ('Nota 2: '))
+score3 = float(input ('Nota 3: '))
+score4 = float(input ('Nota 4: '))
+average = (score1 + score2 + score3 + score4) / 4
+print(average)
