5ae15a7b5e868692c09a1d13e8ae2763de0f381f
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-08-24 19:37:00 -0300

    Activt 19

--- udemy/Student's Guide to Python 3 - Part 2/Accessing Letters + Slicing Strings Exercises.py
+++ udemy/Student's Guide to Python 3 - Part 2/Accessing Letters + Slicing Strings Exercises.py
@@ -1,27 +1,30 @@
-Here are some exercises to try:
+# Here are some exercises to try:
 
+# 1. Set a variable equal to the sentence: "I like birds."
+variable="I like birds."
 
+# 2. Try printing the period in the sentence.
+print(variable[-1])
 
-1. Set a variable equal to the sentence: "I like birds."
+# 3. Try printing the letter 'l' in the sentence.
+print(variable[0])
 
-2. Try printing the period in the sentence.
+# 4. Print each word using slicing strings.
+print(variable[0], variable[2:6], variable[7:12])
 
-3. Try printing the letter 'l' in the sentence.
+# 5. Print the sentence without the period.
+print(variable[:12])
 
-4. Print each word using slicing strings.
-
-5. Print the sentence without the period.
-
-6. Someone has encrypted a message in the string "Ifdlasoasvqwesupjhysdtvfhsmoasn". The key is to get every third letter. Print the decoded message.
-
-
-
-Solution:
-
-sentence = "I like birds."
-print(sentence[-1])
-print(sentence[0])
-print(sentence[0], sentence[2:6], sentence[7:12])
-print(sentence[:12])
+# 6. Someone has encrypted a message in the string "Ifdlasoasvqwesupjhysdtvfhsmoasn". The key is to get every third letter. Print the decoded message.
 code = "Ifdlasoasvqwesupjhysdtvfhsmoasn"
 print(code[::3])
+
+# Solution:
+
+# sentence = "I like birds."
+# print(sentence[-1])
+# print(sentence[0])
+# print(sentence[0], sentence[2:6], sentence[7:12])
+# print(sentence[:12])
+# code = "Ifdlasoasvqwesupjhysdtvfhsmoasn"
+# print(code[::3])
