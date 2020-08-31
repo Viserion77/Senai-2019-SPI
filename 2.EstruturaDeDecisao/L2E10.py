e1c3e372cea85a967b2c6cb40d86d06dd0022691
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-08-31 20:47:30 -0300

    Activity 23

---
+++ 2.EstruturaDeDecisao/L2E10.py
@@ -0,0 +1,12 @@
+# Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+turno = input('Informe o turno em que voce estuda\n[M]atutino\n[V]espertino\n[N]oturno\nOpcao escolhida: ').upper()
+
+if (turno == 'M'):
+    print('Bom dia!')
+elif (turno == 'V'):
+    print('Boa tarde!')
+elif (turno == 'N'):
+    print('Boa noite!')
+else:
+    print('Valar Morgules!')
