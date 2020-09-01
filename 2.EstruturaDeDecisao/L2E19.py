0c372176f07af1678d1f324022a95393cf7ebca4
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-08-31 22:13:43 -0300

    Activity 32

--- 2.EstruturaDeDecisao/L2E19.py
+++ 2.EstruturaDeDecisao/L2E19.py
@@ -2,10 +2,10 @@
 # Observando os termos no plural a colocação do "e", da vírgula entre outros. Exemplo:
 # 326 = 3 centenas, 2 dezenas e 6 unidades
 # 12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
-num = int(input('Informe um numero inteiro: '))
+num = int(input('numero: '))
 
-centenas = num / 100
-dezenas = (num - (centenas * 100)) / 10
+centenas = round(num / 100)
+dezenas = round((num - (centenas * 100)) / 10)
 unidades = (num - (centenas * 100) - (dezenas * 10))
 
 saida = ''
