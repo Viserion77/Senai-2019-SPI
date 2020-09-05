2a9fe3b20aec4290aacd6c406dc784badbbae0e3
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-04 21:27:46 -0300

    activity 3

---
+++ udemy/Lógica de Programação com Dart/aula1/imc.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+main() {
+  print("peso");
+  var text=stdin.readLineSync();
+  var peso=double.parse(text);
+  print("altura");
+  text=stdin.readLineSync();
+  var altura=double.parse(text);
+  print("idade");
+  text=stdin.readLineSync();
+  var idade=int.parse(text);
+  var calc=peso/(altura*idade);
+  if (calc<18.5){
+    print("abaixo");
+  }else if (calc>18.5&&calc<24.9) {
+    print("normal");
+  }else if (calc>25&&calc<29.9) {
+    print("sobrepeso");
+  } else if (calc>30&&calc<34.9) {
+    print("obeso 1");
+  } else if (calc>35&&calc<39.9) {
+    print("obeso 2");
+  } else {
+    print("obeso 3");
+  }
+}
