4c8c87ae6e70ecb442280d6114e7db0f49e10703
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-06 19:19:40 -0300

    Activity 4 and 5

--- udemy/Lógica de Programação com Dart/aula1/imc.dart
+++ udemy/Lógica de Programação com Dart/aula1/imc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 main() {
   print("peso");
@@ -6,18 +7,25 @@ main() {
   print("altura");
   text=stdin.readLineSync();
   var altura=double.parse(text);
-  var calc=peso/(altura*altura);
-  if (calc<18.5){
+  printResult(calcImc(peso,altura));
+}
+
+printResult(double icm){
+  if (icm<18.5){
     print("abaixo");
-  }else if (calc>18.5&&calc<24.9) {
+  }else if (icm>18.5&&icm<24.9) {
     print("normal");
-  }else if (calc>25&&calc<29.9) {
+  }else if (icm>25&&icm<29.9) {
     print("sobrepeso");
-  } else if (calc>30&&calc<34.9) {
+  } else if (icm>30&&icm<34.9) {
     print("obeso 1");
-  } else if (calc>35&&calc<39.9) {
+  } else if (icm>35&&icm<39.9) {
     print("obeso 2");
   } else {
     print("obeso 3");
   }
+}
+
+calcImc(double peso,double altura){
+  return peso/(altura*altura);
 }
