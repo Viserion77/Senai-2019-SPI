5f489ddca0c120799bcadc48b99a2b3efb16e1f9
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-10 19:27:31 -0300

    Activity 36

---
+++ 2.EstruturaDeDecisao/L2E23.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+valor = float(input())
+
+if (valor == int(valor)):
+    print('inteiro')
+else:
+    print('decimal')
