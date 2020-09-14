b4651bc329b4a558e8d4c82bb9ce588d075f00a6
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-14 19:15:35 -0300

    Feat: Creat a API

--- additional-saturday-class/node/index.js
+++ additional-saturday-class/node/index.js
@@ -1,8 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use(express.json());
+
+app.get('/activitys', function (req, res) {
+  res.send(usualResponse(req,res));
+})
+
+app.post('/activitys', function (req, res) {
+  res.send(usualResponse(req,res));
+})
+
+app.delete('/activitys', function (req, res) {
+  res.send(usualResponse(req,res));
 })
  
-app.listen(3000)
+app.listen(3000);
+
+function usualResponse(req,res){
+  let response = [];
+  res.setHeader('Content-Type','application/json');
+  
+  console.log('params',req.params);
+  response.push({params:req.params});
+  
+  console.log('query',req.query);
+  response.push({query:req.query});
+  
+  console.log('headers',req.headers);
+  response.push({headers:req.headers});
+ 
+  console.log('body',req.body);
+  response.push({body:req.body});
+ 
+  console.log('route',req.route);
+  response.push({route:req.route});
+  return response;
+}
