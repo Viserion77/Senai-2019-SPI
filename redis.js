4078d45a41dd0e766a3b558056c8fe9b9a44f97e
viserion77 <jeferson.a.oficial@gmail.com>
2022-02-22 23:22:06 -0300

    refactor

--- redis.js
+++ redis.js
@@ -1,36 +1,38 @@
-const { createConnection } =require('net')
+const { createConnection } = require('net')
 const host = 'redis-16182.c11.us-east-1-3.ec2.cloud.redislabs.com'
 const port = 16182
+const user = 'default'
+const password = 'orO0noPT0zfOPBcxhyf2IYRIAdbwm5bp'
 
-const setCache = async (service, key, value, cacheTime = 60) => {
+const setCache = async (service, key, value) => {
     await new Promise((res, rej) => {
         if (typeof value !== 'string')
             rej('Value should be a string!')
 
         const socket = createConnection({ host, port, })
         socket.once('connect', () => {
-            socket.write({ [`${service}:${key}`]: value }, res)
+            socket.write('+' + JSON.stringify({ [`${service}:${key}`]: value }), res)
+            socket.destroy();
         })
-
-        socket.destroy();
         res(false)
     })
 }
+setCache('1', '2', '3')
 
 const getCache = async (service, key) => {
     await new Promise((res, rej) => {
         const socket = createConnection({ host, port, })
         socket.once('connect', () => {
             socket.on('data', (data) => {
+                res(JSON.parse(data[`${service}:${key}`]))
                 socket.destroy();
-                res(data[`${service}:${key}`])
             });
         })
-        socket.destroy();
         res()
     })
 }
 
+getCache('1', '2')
 module.exports = {
     setCache,
     getCache
