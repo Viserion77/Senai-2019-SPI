66d8a5ccc9483138af4671da5a188d484a205836
viserion77 <jeferson.a.oficial@gmail.com>
2022-02-22 23:09:08 -0300

    refactor rule

--- redis.js
+++ redis.js
@@ -1,15 +1,35 @@
-const redis = require('promise-redis');
-const client = redis.createClient();
+const { createConnection } =require('net')
+const host = 'redis-16182.c11.us-east-1-3.ec2.cloud.redislabs.com'
+const port = 16182
 
 const setCache = async (service, key, value, cacheTime = 60) => {
-    if (typeof value !== 'string')
-        throw new Error('Value should be a string!')
-    const result = await client.set(`${service}:${key}`, value, 'EX', cacheTime);
+    await new Promise((res, rej) => {
+        if (typeof value !== 'string')
+            rej('Value should be a string!')
 
-    return result === 'OK'
+        const socket = createConnection({ host, port, })
+        socket.once('connect', () => {
+            socket.write({ [`${service}:${key}`]: value }, res)
+        })
+
+        socket.destroy();
+        res(false)
+    })
 }
 
-const getCache = async (service, key) => client.get(`${service}:${key}`);
+const getCache = async (service, key) => {
+    await new Promise((res, rej) => {
+        const socket = createConnection({ host, port, })
+        socket.once('connect', () => {
+            socket.on('data', (data) => {
+                socket.destroy();
+                res(data[`${service}:${key}`])
+            });
+        })
+        socket.destroy();
+        res()
+    })
+}
 
 module.exports = {
     setCache,
